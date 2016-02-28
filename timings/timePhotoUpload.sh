125f7ce2507a6a5199f9c1f31cd55191849f61e2
Jake <jr2812@ic.ac.uk>
2016-02-28 15:39:04 +0000

    Added timePhotoUpload.sh

---
+++ timings/timePhotoUpload.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+BASEURL=$1
+TOTAL=0
+ENDPOINT="/imageUpload"
+echo $(ls -l $2)
+for i in {1..10}
+do
+	LATEST=$2
+	URL=$BASEURL$ENDPOINT
+	START=$(date +%s%3N)
+	curl -POST -s -F "image=@${LATEST}"  $URL -o /dev/null
+	END=$(date +%s%3N)
+	let DURATION=$END-$START
+	let TOTAL=$TOTAL+$DURATION
+	#echo $DURATION
+	sleep 1
+done
+let AVERAGE=$TOTAL/10
+echo "Average Upload: "$AVERAGE" milliseconds"
