baafd26bbc206322aa36c704a6e5a9c43ff292fb
Jake <jr2812@ic.ac.uk>
2016-02-28 17:34:10 +0000

    Modded timePhotoUpload output

--- timings/timePhotoUpload.sh
+++ timings/timePhotoUpload.sh
@@ -1,21 +1,26 @@
-
 #!/bin/bash
 
 BASEURL=$1
-TOTAL=0
 ENDPOINT="/imageUpload"
-echo $(ls -l $2)
-for i in {1..10}
+PHOTODIR="testPhotos"
+
+for i in $(ls $PHOTODIR)
 do
-	LATEST=$2
-	URL=$BASEURL$ENDPOINT
-	START=$(date +%s%3N)
-	curl -POST -s -F "image=@${LATEST}"  $URL -o /dev/null
-	END=$(date +%s%3N)
-	let DURATION=$END-$START
-	let TOTAL=$TOTAL+$DURATION
-	#echo $DURATION
-	sleep 1
+	TOTAL=0
+	stat -c "Name: %n        Size: %s" $PHOTODIR"/"$i
+	for j in {1..10}
+	do
+		URL=$BASEURL$ENDPOINT
+		START=$(date +%s%3N)
+		curl -POST -s  -F "image=@${PHOTODIR}/${i}"  $URL -o /dev/null
+		END=$(date +%s%3N)
+		let DURATION=$END-$START
+		echo -n $DURATION"," 
+		let TOTAL=$TOTAL+$DURATION
+		sleep 1
+	done
+	let AVERAGE=$TOTAL/10
+	echo ""
+	echo "Average Upload: "$AVERAGE" milliseconds"
+	echo ""
 done
-let AVERAGE=$TOTAL/10
-echo "Average Upload: "$AVERAGE" milliseconds"
