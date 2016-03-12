0d80ce7433a3b1c9b99ae546c3bbd6d0b98ff216
Jake <jr2812@ic.ac.uk>
2016-03-12 20:03:03 +0000

    Changed photo name format

--- takePhoto.sh
+++ takePhoto.sh
@@ -5,7 +5,8 @@
 
 while [ 1 ]
 do
-	DATE=$(date +"%Y-%m-%d_%H%M")
+	#DATE=$(date +"%Y-%m-%d_%H%M")
+	DATE=$(($(date +%s%N)/1000000))
 	echo "Taking shot.."
 	raspistill  -h 480 -w 640 -t 1 -o /home/pi/drone_pi/photos/$DATE.jpg
 	echo "Done."
