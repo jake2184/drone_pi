e8f833096efb8755579446703180d878404e16e9
Jake Reynolds <jakealexis@gmail.com>
2016-05-24 18:30:06 +0100

    photoLatency all works

--- test/photoLatency.sh
+++ test/photoLatency.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
-curl -X POST -u jake:pass 192.168.1.77:8080/login -c cookie
+URL=192.168.1.77:8080
+
+curl -X POST -u jake:pass $URL/login -c cookie
 
 
 for i in $( ls photos );
 do
-	curl -b cookie  -X POST -F "image=@photos/${i}" 192.168.1.77:8080/api/test/imageLatency/${i}
+	DATE=$(($(date +%s%N)/1000000))
+	curl -b cookie  -X POST -F "image=@photos/${i}" $URL/api/test/imageLatency/${i} -F time=${DATE}
 done
+
+rm cookie
