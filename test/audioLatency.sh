142c4b7313593b57e3794804568b70c74b0439ad
Jake Reynolds <jakealexis@gmail.com>
2016-05-31 16:37:53 +0100

    Do file upload 10 times

--- test/audioLatency.sh
+++ test/audioLatency.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
-URL=192.168.1.77:8080
+URL=192.168.1.76:8080
 #URL=https://drone-nodes.eu-gb.mybluemix.net
 curl -X POST -u jake:pass $URL/login -c cookie -d ""
 
-
-for i in $( ls audio );
+for i in {1..10}
 do
-	DATE=$(($(date +%s%N)/1000000))
-	curl -b cookie  -X POST -F "audio=@audio/${i}" $URL/api/test/audioLatency/${i} -F time=${DATE}
+    for i in $( ls audio );
+    do
+        DATE=$(($(date +%s%N)/1000000))
+        curl -b cookie  -X POST -F "audio=@audio/${i}" $URL/api/test/audioLatency/${i} -F time=${DATE}
+    done
 done
+sleep 20
+curl -b cookie $URL/api/test/writeAudioFile
 
 rm cookie
--- test/photoLatency.sh
+++ test/photoLatency.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 
-URL=192.168.1.77:8080
+URL=192.168.1.76:8080
 #URL=https://drone-nodes.eu-gb.mybluemix.net
 curl -X POST -u jake:pass $URL/login -c cookie -d ""
 
-
-for i in $( ls photos );
+for i in {i..10}
 do
-	DATE=$(($(date +%s%N)/1000000))
-	curl -b cookie  -X POST -F "image=@photos/${i}" $URL/api/test/imageLatency/${i} -F time=${DATE}
+    for i in $( ls photos );
+    do
+        DATE=$(($(date +%s%N)/1000000))
+        curl -b cookie  -X POST -F "image=@photos/${i}" $URL/api/test/imageLatency/${i} -F time=${DATE}
+    done
 done
+sleep 20
+curl -b cookie $URL/api/test/writeImageFile
 
 rm cookie
